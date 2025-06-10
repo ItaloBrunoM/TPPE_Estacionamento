d2f90124a020bf13416711234d8ec4359445dd0b
ItaloBrunoM <italo23042001@gmail.com>
2025-06-10 17:53:25 -0300

    refactor(database): refatoração da database

--- database/02_inserirTabelas.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO VAGAS (tipo) VALUES 
-('COMUM'), ('COMUM'), ('IDOSO'), ('DEFICIENTE')
-ON CONFLICT DO NOTHING;
-
-INSERT INTO CARROS (placa, modelo, cor, vaga_id) VALUES 
-('ABC1D23', 'Fiat Uno', 'Vermelho', 1),
-('XYZ9E87', 'Honda Civic', 'Prata', 2)
-ON CONFLICT (placa) DO NOTHING;
