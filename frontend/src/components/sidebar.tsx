602fe94c309408ee6baace075d66d555beba953e
ItaloBrunoM <italo23042001@gmail.com>
2025-06-11 00:49:46 -0300

    refactor(frontend):refatoração com lint sidebar

--- frontend/src/components/sidebar.tsx
+++ frontend/src/components/sidebar.tsx
@@ -1,5 +1,5 @@
-import { NavLink } from 'react-router-dom';
-import './sidebar.css';
+import { NavLink } from "react-router-dom";
+import "./sidebar.css";
 
 interface SidebarProps {
   userName: string;
@@ -8,16 +8,18 @@ interface SidebarProps {
 }
 
 export function Sidebar({ userName, userRole, isVisible }: SidebarProps) {
-  const getLinkClass = ({ isActive }: { isActive: boolean }) => 
-    isActive ? 'sidebar-link active' : 'sidebar-link';
+  const getLinkClass = ({ isActive }: { isActive: boolean }) =>
+    isActive ? "sidebar-link active" : "sidebar-link";
 
   return (
-    <nav className={`sidebar ${isVisible ? '' : 'hidden'}`}> {/* Aplica a classe 'hidden' com base em isVisible */}
+    <nav className={`sidebar ${isVisible ? "" : "hidden"}`}>
+      {" "}
+      {/* Aplica a classe 'hidden' com base em isVisible */}
       <div className="sidebar-header">
         <h3>ESTACIONAMENTO TOP</h3>
       </div>
       <div className="sidebar-links">
-      <NavLink to="/" end className={getLinkClass}>
+        <NavLink to="/" end className={getLinkClass}>
           VISÃO GERAL
         </NavLink>
         <NavLink to="/estacionamentos" className={getLinkClass}>
