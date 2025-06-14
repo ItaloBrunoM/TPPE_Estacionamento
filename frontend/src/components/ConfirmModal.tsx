7fd65e6ef691dfd3e7a0c8de9367719744d2763c
ItaloBrunoM <italo23042001@gmail.com>
2025-06-14 00:54:36 -0300

    refactor(frontend): adição do pretier no confirmModal

--- frontend/src/components/ConfirmModal.tsx
+++ frontend/src/components/ConfirmModal.tsx
@@ -1,4 +1,4 @@
-import './ConfirmModal.css';
+import "./ConfirmModal.css";
 
 interface ConfirmModalProps {
   message: string;
@@ -6,14 +6,22 @@ interface ConfirmModalProps {
   onCancel: () => void;
 }
 
-export const ConfirmModal = ({ message, onConfirm, onCancel }: ConfirmModalProps) => {
+export const ConfirmModal = ({
+  message,
+  onConfirm,
+  onCancel,
+}: ConfirmModalProps) => {
   return (
     <div className="modal-overlay">
       <div className="modal-content">
         <p>{message}</p>
         <div className="modal-actions">
-          <button onClick={onConfirm} className="btn-confirm">Confirmar</button>
-          <button onClick={onCancel} className="btn-cancel">Cancelar</button>
+          <button onClick={onConfirm} className="btn-confirm">
+            Confirmar
+          </button>
+          <button onClick={onCancel} className="btn-cancel">
+            Cancelar
+          </button>
         </div>
       </div>
     </div>
