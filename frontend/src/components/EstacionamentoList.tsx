4ec5662083ed699f0dd166422273deb36a6891da
ItaloBrunoM <italo23042001@gmail.com>
2025-06-14 00:56:52 -0300

    refactor(frontend): adição do pretier no EstacionamentoList

--- frontend/src/components/EstacionamentoList.tsx
+++ frontend/src/components/EstacionamentoList.tsx
@@ -4,7 +4,7 @@ import "./EstacionamentoList.css";
 interface EstacionamentoListProps {
   estacionamentos: EstacionamentoType[];
   onAddClick: () => void;
-  onDeleteItemClick: (estacionamento: EstacionamentoType) => void; 
+  onDeleteItemClick: (estacionamento: EstacionamentoType) => void;
   onEditClick: (estacionamento: EstacionamentoType) => void;
 }
 
@@ -14,12 +14,15 @@ export function EstacionamentoList({
   onDeleteItemClick,
   onEditClick,
 }: EstacionamentoListProps) {
-
   return (
     <>
       <div
         className="header-actions"
-        style={{ display: "flex", justifyContent: "space-between", marginBottom: "20px" }}
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          marginBottom: "20px",
+        }}
       >
         <input type="search" placeholder="Buscar Estacionamento" />
         <button onClick={onAddClick} style={{ cursor: "pointer" }}>
@@ -32,12 +35,25 @@ export function EstacionamentoList({
           <div
             key={est.id}
             className="list-item"
-            style={{ display: "flex", justifyContent: "space-between", padding: "10px", border: "1px solid #ccc", marginBottom: "10px" }}
+            style={{
+              display: "flex",
+              justifyContent: "space-between",
+              padding: "10px",
+              border: "1px solid #ccc",
+              marginBottom: "10px",
+            }}
           >
             <span className="item-name">{est.nome}</span>
             <div className="item-actions">
-              <button className="icon-btn" onClick={() => onEditClick(est)}>✏️</button>
-              <button className="icon-btn" onClick={() => onDeleteItemClick(est)}>🗑️</button>
+              <button className="icon-btn" onClick={() => onEditClick(est)}>
+                ✏️
+              </button>
+              <button
+                className="icon-btn"
+                onClick={() => onDeleteItemClick(est)}
+              >
+                🗑️
+              </button>
             </div>
           </div>
         ))}
